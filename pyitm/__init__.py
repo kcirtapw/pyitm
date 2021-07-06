aadb6d1ac417892fbdc77e2ea66d41abd3752e23
Patrick Welzel <git@kcirtap.de>
2021-07-06 22:20:38 +0200

    fix module for flit

--- pyitm/__init__.py
+++ pyitm/__init__.py
@@ -1,2 +1,6 @@
+"""An easy to use, TCP and UDP level Man-in-the-middle framework intended for security researchers and practitioners."""
+
+__version__ = "0.9"
+
 from .main import setupPyITMUdp, setupPyITMTcp, Tap
 from .example_taps import PrintTap, ReReplaceTap
